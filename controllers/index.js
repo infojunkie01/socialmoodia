74200c9fb9f57da7d76abd9e1e87585b44fb8288
Abby Oliva <abbyoliva6@icloud.com>
2022-06-03 15:02:51 -0600

    profile, home, and user page routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const userPageRoutes = require('./user-page-routes');
+const profileRoutes = require('./profile-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/user-page', userPageRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
