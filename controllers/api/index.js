c49cebc7f8e1465c1242daad701052c794652974
Abby Oliva <abbyoliva6@icloud.com>
2022-06-03 14:57:55 -0600

    html routes folder created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
 
