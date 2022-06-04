f438215f70d0f00eaab49d8d806a5d79792af41b
Jessica Wang <jessica.l.wang01@gmail.com>
2022-06-03 22:35:53 -0400

    sadds to user-routes.js and home-routes.js, note that Follow model is not used yet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,91 @@
 const router = require('express').Router();
 
+const { Post, User, Comment, Follow } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+    
+    Post.findAll({
+      attributes: [
+        'id',
+        'body',
+        'user_id'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'body', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'body',
+        'user_id'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'body', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username'  ]
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+
 module.exports = router;
+
