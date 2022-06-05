69db312e8a0969588051db86113bb6cbab87a50a
Abby Oliva <abbyoliva6@icloud.com>
2022-06-05 12:30:15 -0600

    user-page handlebars

---
+++ public/javascript/add-post.js
@@ -0,0 +1,23 @@
+async function addPost(event) {
+    event.preventDefault();
+
+    const post_content = document.querySelector('input[name="add-post-text"]').value;
+  
+    const response = await fetch(`/api/post`, {
+      method: 'POST',
+      body: JSON.stringify({
+        post_content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+}
+  
+document.querySelector('#add-post-button').addEventListener('submit', addPost);
