78b91a71e38aa8201037c7bdf8cf0b651ba5d788
Zmaha106 <mahabubzinia1@gmail.com>
2022-06-05 15:06:36 -0400

    added code to the new file

--- controllers/user-name-routes.js
+++ controllers/user-name-routes.js
@@ -1,124 +1,50 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Follows } = require('../models');
+const { Post, User, Comment, Follow } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, (req, res) => {
+
+
+router.get('/:username', withAuth (req, res) => {
     Post.findAll({
-      where: {
-        // use the ID from the session
-        user_id: req.session.user_id
-      },
-      attributes: [
-        'id',
-        'body'
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'body', 'post_id', 'user_id'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
+        where: {
+            username: req.session.username
         },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
+        attributes: [
+            'id',
+            'user_id'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username', 'password' ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
     })
-      .then(dbPostData => {
-        // serialize data before passing to template
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('user-page', { posts, loggedIn: true });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-  router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'body'
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'body', 'post_id', 'user_id'],
-          include: {
-            model: User,
-            attributes: ['username' ]
-          }
-        },
-        {
-          model: User,
-          attributes: ['username' ]
+    .then(dbUserData) => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'no username found' });
+            return;
         }
-      ]
-    })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-  
-        // serialize the data
-        const post = dbPostData.get({ plain: true });
+    }
+}
+//     .get_user(req.params.username, function (err, results) {
+//         if(results[0]) {
+//             res.render('/profile', {
+//                 title: 'Profile',
+//                 userinfo: results[0]
+//             });
+//         } else {
+//             next();
+//         }
+//     })
+// );
 
-        res.render('edit-post', {
-            post,
-            loggedIn: true
-            });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-});
-
-router.get('/create/', withAuth, (req, res) => {
-    Post.findAll({
-      where: {
-        // use the ID from the session
-        user_id: req.session.user_id
-      },
-  
-      attributes: [
-        'id',
-        'body'
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'body', 'post_id', 'user_id'],
-          include: {
-            model: User,
-            attributes: ['username' ]
-          }
-        },
-        {
-          model: User,
-          attributes: ['username' ]
-        }
-      ]
-    })
-      .then(dbPostData => {
-        // serialize data before passing to template
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('new-post', { posts, loggedIn: true });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
 
 
 module.exports = router;
