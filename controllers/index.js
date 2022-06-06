6cc23b274eb2b104a2b0300e5502dc69a5874974
Zmaha106 <mahabubzinia1@gmail.com>
2022-06-05 22:01:05 -0400

    commented out user-name-routes temporarily

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes');
 const userPageRoutes = require('./user-page-routes');
 const profileRoutes = require('./profile-routes');
+// const userNameRoutes = require('/user-name-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/user-page', userPageRoutes);
 router.use('/profile', profileRoutes);
+// router.use('/user-name', userNameRoutes);
 
 module.exports = router;
