9fbff6fdb90c33c3a2162ab9e10fdd878ce82fe3
unknown <russelhkamal@gmail.com>
2022-06-05 22:26:36 -0400

    fixed triple post problem

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,6 +1,6 @@
 async function addPost(event) {
     event.preventDefault();
-
+    
     const body = document.querySelector('input[name="add-post-text"]').value.trim();
    
     const response = await fetch(`/api/post`, {
@@ -11,14 +11,16 @@ async function addPost(event) {
       headers: {
         'Content-Type': 'application/json'
       }
+      
     });
+    
   
     if (response.ok) {
       document.location.replace('/');
     } else {
       alert(response.statusText);
     }
+
   }
 
-document.querySelector('.new-post-form').addEventListener('submit', addPost);
-
+ $(".new-post-form").off().submit(addPost);
