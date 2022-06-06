78b2f5f7605b7a99e7236112455e4e79ed24367c
Abby Oliva <abbyoliva6@icloud.com>
2022-06-06 13:50:23 -0600

    comment counter

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,13 +9,13 @@ router.get('/', (req, res) => {
         attributes: [
           'id',
           'body',
-          // 'created_at'
+          //'created_at'
         ],
         include: [
           {
             model: Comment,
             attributes: ['id', 'comment_text', 'post_id',  'user_id'],
-            // attributes: ['id', 'comment_text', 'post_id',  'user_id', 'created_at'],
+            //attributes: ['id', 'comment_text', 'post_id',  'user_id', 'created_at'],
             include: {
               model: User,
               attributes: ['username']
@@ -70,13 +70,13 @@ router.get('/', (req, res) => {
       attributes: [
         'id',
         'body',
-        // 'created_at'
+        //'created_at'
       ],
       include: [
         {
           model: Comment,
           attributes: ['id', 'comment_text', 'post_id', 'user_id'],
-          // attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          //attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
             attributes: ['username']
