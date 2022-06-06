b2d26a1b2d93041fb8d63f415d7605462717c0bd
Jessica Wang <jessica.l.wang01@gmail.com>
2022-06-06 19:22:17 -0400

    adds to profile-routes.js

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,3 +1,43 @@
 const router = require('express').Router();
+const { Post, User, Comment, Follows } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        // use the ID from the session
+        username: req.params.username
+      },
+      attributes: [
+        'id',
+        'body',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('user-page', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
